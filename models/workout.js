fa6a8aedb2d626d8fdc4a5691676e9a06468a5ac
Brittany <britloy91@gmail.com>
2020-02-25 20:06:06 -0500

    heroku update

--- models/workout.js
+++ models/workout.js
@@ -7,7 +7,7 @@ const WorkoutSchema = new Schema({
         type: Date,
         default: Date.now
     },
-    exercise: [{
+    exercises: [{
         
     
         name: {
@@ -54,8 +54,8 @@ const WorkoutSchema = new Schema({
 
 WorkoutSchema.virtual("totalDuration").get(function () {
     // "reduce" array of exercises down to just the sum of their durations
-    return this.exercise.reduce((total, exercise) => {
-        return total + exercise.duration;
+    return this.exercises.reduce((total, exercises) => {
+        return total + exercises.duration;
     }, 0);
 
 });
